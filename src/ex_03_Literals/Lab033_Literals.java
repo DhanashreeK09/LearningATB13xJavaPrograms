ed10df825b9ae9588f48b4bd5e1d94eafb67e462
Dhanashree Khadamkar <dhanashree.khadamkar@gmail.com>
2025-06-26 10:53:44 +0530

    26th June 2025

---
+++ src/ex_03_Literals/Lab033_Literals.java
@@ -0,0 +1,15 @@
+package ex_03_Literals;
+
+public class Lab033_Literals {
+    public static void main(String[] args) {
+        int age = 65;
+         // Binary Literals
+        int binary_num = 0b1010; // decimal number 10;
+        //Octal base ->8
+        int octal = 0101;
+        //Hexadecimal literals
+        int hex = 0Xface;
+        // base 16 color combination are in hex #28a745
+        // above literals are never used in Automation - FYI
+    }
+}
---
+++ src/ex_03_Literals/Lab034_Char_Literals.java
@@ -0,0 +1,8 @@
+package ex_03_Literals;
+
+public class Lab034_Char_Literals {
+    public static void main(String[] args) {
+        char c = "A";
+
+    }
+}
