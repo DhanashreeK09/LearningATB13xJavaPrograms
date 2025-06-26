309233c5aabbb8ecb20744410e9346f008ae3e0f
Dhanashree Khadamkar <dhanashree.khadamkar@gmail.com>
2025-06-26 11:31:01 +0530

    26th June 2025

---
+++ src/ex_03_Literals/Lab035_Non_Primitive_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Primitive_Literals {
+    public static void main(String[] args) {
+        // non premitive data types are defined by users and size is not defined
+        String name  = "Dhanashree"; // String is bunch of char
+        int[] array_of_items = new int[10];
+        String s = null; // only string can have null value
+
+    }
+}
---
+++ src/ex_03_Literals/Task1_Print_table9_using_printf.java
@@ -0,0 +1,15 @@
+package ex_03_Literals;
+
+public class Task1_Print_table9_using_printf {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("%d * %d = %d", 9,1,9);
+        System.out.println();
+        System.out.printf("%d * %d = %d", 9,2,18).println();
+        System.out.printf("%d * %d = %d", 9,3,27).println();
+        System.out.printf("%d * %d = %d", 9,4,36).println();
+        System.out.print("...");
+        System.out.printf("%d * %d = %d", 9,10,90);
+
+    }
+}
